8df4e7902b865c8d7a4b221189e645ef343ca1d0
Edigleison <edigleisonalves@gmail.com>
2016-04-07 23:30:31 -0300

    create user service

---
+++ src/br/com/edigleison/estudos/soa/services/UsersService.java
@@ -0,0 +1,18 @@
+package br.com.edigleison.estudos.soa.services;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/users")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public interface UsersService {
+	
+	@GET
+	Response getUsers();
+
+}
