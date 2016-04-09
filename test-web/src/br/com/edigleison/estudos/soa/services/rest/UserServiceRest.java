3ceddbb6596311bdb39238a92562a406af78caef
Edigleison <edigleisonalves@gmail.com>
2016-04-08 22:11:29 -0300

    reestruturação do projeto

---
+++ test-web/src/br/com/edigleison/estudos/soa/services/rest/UserServiceRest.java
@@ -0,0 +1,26 @@
+package br.com.edigleison.estudos.soa.services.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import br.com.edigleison.estudos.soa.model.User;
+import br.com.edigleison.estudos.soa.services.UsersService;
+
+public class UserServiceRest implements UsersService{
+
+	private List<User> users = new ArrayList<User>();
+	
+	
+	@Override
+	public Response getUsers() {
+//		if(users.isEmpty()){
+//			return Response.status(Status.NOT_FOUND).build();
+//		}
+		
+		return Response.ok(users).build();
+	}
+
+}
